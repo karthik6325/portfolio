e1c95ec486f4895bf018f1870d55e58ac579f451
karthik <karthik63254@gmail.com>
2024-02-26 15:19:22 +0530

    updates to contact and projects

--- src/components/form.js
+++ src/components/form.js
@@ -1,20 +1,30 @@
 import './form.css';
-import React from 'react'
+import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
-const form = () => {
+const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.error('Error sending message!');
+    e.target.reset();
+  };
+
   return (
     <div className='form'>
-        <form>
-      <label>Your Name</label>
-      <input type='text'></input>
-      <label>Email</label>
-      <input type='text'></input>
-      <label>Message</label>
-      <textarea rows='6' placeholder='Type your message here'/>
-      <button className='btn'>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <label>Your Name</label>
+        <input type='text' name='name' />
+        <label>Email</label>
+        <input type='text' name='email' />
+        <label>Message</label>
+        <textarea rows='6' name='message' placeholder='Type your message here' />
+        <button type='submit' className='btn'>
+          Submit
+        </button>
+        <Toaster />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default form
+export default Form;
--- src/components/workcarddata.js
+++ src/components/workcarddata.js
@@ -1,6 +1,7 @@
 import Pro1 from '../assets/pro1.jpg';
 import Pro2 from '../assets/pro2.jpg';
 import Pro3 from '../assets/pro3.PNG';
+import Pro4 from '../assets/pro4.PNG';
 
 const ProjectCardData = [
     {
@@ -20,6 +21,12 @@ const ProjectCardData = [
         title: "Movies_App",
         text: "It is a React application for movie enthusiasts, featuring updated lists of popular, top-rated, and upcoming movies. It uses the movie database (TMDB) Api to fetch the required data.",
         view: "https://github.com/karthik6325/Movies_app",
+    },
+    {
+        imgsrc: Pro4,
+        title: "Find-Donor",
+        text: "I developed a MERN web application that connects blood donors with those in need. Any user can create an account, after creation of account users can register as donors or seekers, storing theirinformation in a database. When seekers search for donors based on city and blood type, the application instantly displays a list of matching donors.",
+        view: "https://github.com/karthik6325/FindDonor",
     },
 ];
 
